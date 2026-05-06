68bd672b398932d7e87a95fe67113a5032d5c6ee
Musfiqur Rahaman <musfiqurahaman987@gmail.com>
2026-05-06 12:32:54 -0700

    Implement a REPL

--- app/main.py
+++ app/main.py
@@ -2,10 +2,12 @@ import sys
 
 
 def main():
-    sys.stdout.write("$ ")
-    
-    command = input()
-    print(f"{command}: command not found")
+
+    while True:
+        sys.stdout.write("$ ")
+
+        command = input()
+        print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
